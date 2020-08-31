d6103d69e49d1c3331bd5092945d90d0c2fcc4a5
Shay Reichert <63343303+Kurtzs@users.noreply.github.com>
2020-08-31 16:32:27 +0200

    includes before regex modif

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,20 +1,21 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class Display extends Component {
-
-    render() {
-        let { result } = this.props;
-        return (
-            <div>
-                <div id="display-shadow">
-                    <div id="display">
-                        {result}
-                    </div>
+const Display = (props) => {
+    const { stockNumber, result } = props;
+    return (
+        <div>
+            <div id="display-shadow">
+                <div id="display">
+                    <h2>{stockNumber} </h2>
+                    <h1>{result}</h1>
                 </div>
-
             </div>
-        )
-    }
+        </div>
+    )
+
 }
+
+
+
 
 export default Display
